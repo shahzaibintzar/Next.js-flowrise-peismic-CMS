b8272f003f4998ee834b3aa438fb4629626131e9
SHAHZAIB <shahzaibintzar27346@gmail.com>
2024-05-07 12:52:43 +0500

    Style Header and Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,25 +1,31 @@
 import { createClient } from "@/prismicio";
 import { PrismicLink } from "@prismicio/react";
 import Link from "next/link";
-
+import Logo from "@/components/Logo";
+import Bounded from "./Bounded";
 
 export default async function Footer(){
     const client = createClient();
 
     const setting = await client.getSingle("setting");
     return (
-      <footer>
-        <Link href={"/"}>{setting.data.meta_description}</Link>
-        <p>
+      <Bounded as="footer">
+        <div className="flex sm:flex-row flex-col justify-between items-center gap-6">
+
+        <Link href={"/"}>
+          <Logo />
+          </Link>
+        <p className="text-xs">
           @{new Date().getFullYear()} {setting.data.meta_description}
         </p>
-        <ul>
+        <ul className="flex">
           {setting.data.navigation.map(({ link, label }) => (
             <li key={label}>
-              <PrismicLink field={link}>{label} </PrismicLink>
+              <PrismicLink field={link} className="p-3">{label} </PrismicLink>
             </li>
           ))}
         </ul>
-      </footer>
+          </div>
+      </Bounded>
     );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,24 +1,31 @@
 import { createClient } from "@/prismicio";
 import { PrismicLink } from "@prismicio/react";
 import Link from "next/link";
+import Bounded from "@/components/Bounded";
+import Logo from "@/components/Logo"
 
 export default async function Header() {
     const client = createClient();
 
     const setting = await client.getSingle("setting");
 
-  return <header>
+  return (
+  <Bounded as="header" className="py-4 md:py-6 lg:py-8">
+    <div className="flex gap-4 items-center justify-between sm:flex-row flex-col">
+
     <Link href={"/"}>
-    {setting.data.meta_description}
+    <Logo />
     </Link>
   <nav>
-    <ul>
+    <ul className="flex">
         {setting.data.navigation.map(({link,label}) =>(
-            <li key={label}>
-            <PrismicLink field={link}>{label}</PrismicLink>
+          <li key={label}>
+            <PrismicLink field={link} className="p-3">{label}</PrismicLink>
             </li>
         ))}
     </ul>
   </nav>
-  </header>;
+    </div>
+  </Bounded>
+  )
 }
